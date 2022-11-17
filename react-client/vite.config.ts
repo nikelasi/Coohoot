465f6dacac63caa7fdbe953ee23c7b1685a2905a
NicholasJohansan <nicholasjohansan@gmail.com>
2022-11-17 12:43:50 +0800

    Set up HMR in dockerized react app

--- react-client/vite.config.ts
+++ react-client/vite.config.ts
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    hmr: {
+      clientPort: 3000
+    },
+    port: 3000,
+    watch: {
+      usePolling: true
+    }
+  }
 })
