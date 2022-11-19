8a16b5475ced54490786aeff3dff9daccf8d8d7b
NicholasJohansan <nicholasjohansan@gmail.com>
2022-11-19 15:50:52 +0800

    Implement mail function

---
+++ laravel-server/app/Mail/TestMail.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+// Usage:
+// Mail::to(<emaiL>)->send(new App\Mail\TestMail(<email>));
+
+class TestMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public string $email;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(string $email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * Get the message envelope.
+     *
+     * @return \Illuminate\Mail\Mailables\Envelope
+     */
+    public function envelope()
+    {
+        return new Envelope(
+            subject: 'Test Mail',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     *
+     * @return \Illuminate\Mail\Mailables\Content
+     */
+    public function content()
+    {
+        return new Content(
+            view: 'emails.test',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array
+     */
+    public function attachments()
+    {
+        return [];
+    }
+}
---
+++ laravel-server/resources/views/emails/test.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Test Email</title>
+    <style>
+      .header {
+        color: red;
+      }
+    </style>
+</head>
+<body>
+    <h1 class="header">Test Email</h1>
+    <p>Hi {{ $email }}, this is a test email from Coohoot's server.</p>
+</body>
+</html>
