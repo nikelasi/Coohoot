84d0756ce9039cf190768569dc64ceae97695af2
NicholasJohansan <nicholasjohansan@gmail.com>
2022-11-20 19:48:30 +0800

    Repurpose password_resets table to be for tokens in general

--- laravel-server/database/migrations/2_create_user_tokens_table.php
+++ laravel-server/database/migrations/2_create_user_tokens_table.php
@@ -13,9 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('token');
+        Schema::create('user_tokens', function (Blueprint $table) {
+            $table->uuid('id')->primary();
             $table->uuid('user_id');
+            $table->string('token');
+            $table->enum('type', ['password_reset', 'email_verification']);
             $table->timestamp('expires_at');
 
             $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete();
@@ -29,6 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('user_tokens');
     }
 };
