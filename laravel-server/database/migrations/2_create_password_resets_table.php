3e8a846ee589d10e96fc82a42c695cf07fa7ca1b
NicholasJohansan <nicholasjohansan@gmail.com>
2022-11-20 18:23:50 +0800

    Add password resets table migration

---
+++ laravel-server/database/migrations/2_create_password_resets_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('password_resets', function (Blueprint $table) {
+            $table->string('token');
+            $table->uuid('user_id');
+            $table->timestamp('expires_at');
+
+            $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('password_resets');
+    }
+};
