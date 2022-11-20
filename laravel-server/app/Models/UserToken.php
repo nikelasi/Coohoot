d0383d1513ffcb26eab035d618bbad73e1811510
NicholasJohansan <nicholasjohansan@gmail.com>
2022-11-20 19:49:34 +0800

    Make UserToken model

---
+++ laravel-server/app/Models/UserToken.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use HasUuids;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Prunable;
+
+class UserToken extends Model
+{
+    use HasUuids;
+    use Prunable;
+
+    protected $table = 'user_tokens';
+
+    protected static function boot() {
+        parent::boot();
+
+        static::creating(function ($model) {
+            $model->token = bin2hex(random_bytes(32));
+            $model->expires_at = now()->addMinutes(10);
+        });
+    }
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+    
+    public function prunable() {
+        return static::where('expires_at', '<', now());
+    }
+}
