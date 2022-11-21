ab4e02098bdd6b2ea0668c58408029361759021f
NicholasJohansan <nicholasjohansan@gmail.com>
2022-11-21 17:13:59 +0800

    Add AuthService

---
+++ laravel-server/app/Services/AuthService.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Services;
+
+use GoogleDriveService;
+
+use App\Models\User;
+use App\Models\UserToken;
+
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
+
+class AuthService {
+
+  public function __construct() {}
+
+  /**
+   * Registers a new user.
+   * 
+   * @param array[
+   *  'username' => string,
+   *  'email' => string,
+   *  'password' => string,
+   *  'pfp' => string
+   * ] $user
+   * @return void
+   */
+  public function registerUser($user, $pfp) {
+    $user = User::create([
+      'username' => $user['username'],
+      'email' => $user['email'],
+      'password' => Hash::make($user['password']),
+      'pfp_url' => GoogleDriveService::uploadImage($pfp)
+    ]);
+
+    $this->createVerificationToken($user->id, 'email_verification');
+  }
+
+  /**
+   * Creates a email verification token for the given user.
+   * 
+   * @param string $uuid The user's UUID
+   * @return void
+   */
+  public function createVerificationToken($uuid) {
+    $user = User::find($uuid);
+
+    if ($user->verified) {
+      return;
+    }
+    
+    $token = UserToken::create([
+      'user_id' => $id,
+      'type' => 'email_verification'
+    ]);
+
+    // Mail::to($user->email)->send(new EmailVerificationMail($user, $token));
+  }
+
+  /**
+   * Attempt to verify the user's email with given token.
+   * 
+   * @param string $token The user's email verification token
+   * @return bool Whether verification was successful
+   */
+  public function verifyUser($token) {
+    $token = UserToken::where('token', $token)->first();
+
+    if ($token) {
+      $user = User::find($token->user_id);
+      $user->verified = true;
+      $user->save();
+
+      $token->delete();
+
+      return true;
+    }
+
+    return false;
+  }
+
+  /**
+   * Attempt to login the user with given credentials.
+   * 
+   * @param array[
+   *  'username' => string,
+   *  'email' => string,
+   *  'password' => string,
+   * ] $credentials
+   * @return (string|null) A token if the attempt was successful, null otherwise
+   */
+  public function login($credentials) {
+
+    if (isset($credentials['username'])) {
+      $user = User::where('username', $credentials['username'])->first();
+    } else if (isset($credentials['email'])) {
+      $user = User::where('email', $credentials['email'])->first();
+    }
+
+    if ($user && Hash::check($credentials['password'], $user->password)) {
+      $token = auth()->login($user);
+      return $token;
+    }
+
+    return null;
+  }
+
+}
