556b0ca4619642e4ea835a3b15df5a9de5b6c482
NicholasJohansan <nicholasjohansan@gmail.com>
2022-12-12 19:35:46 +0800

    Style 404 page

--- react-client/src/pages/NotFound.tsx
+++ react-client/src/pages/NotFound.tsx
@@ -1,9 +1,21 @@
+import { Flex, Heading, Text, VStack } from "@chakra-ui/react"
+
+import CoohootOwl from '../assets/svg/CoohootOwl.svg'
 
 const NotFound: React.FC = () => {
   return (
-    <div>
-      <h1>404 Page</h1>
-    </div>
+    <Flex
+      justifyContent="center"
+      alignItems="center"
+      h="calc(100vh - 4rem)"
+      w="100vw"
+    >
+      <VStack>
+        <CoohootOwl boxSize="32" />
+        <Heading fontSize="6xl">coo... 404</Heading>
+        <Text fontSize="3xl" color="brand">Page Not Found</Text>
+      </VStack>
+    </Flex>
   )
 }
 
