2f919480bba60b27c06d22ba8e5a51eabb3b44a9
NicholasJohansan <nicholasjohansan@gmail.com>
2022-12-13 22:26:09 +0800

    Ensure username is unique in schema

--- laravel-server/database/migrations/1_create_users_table.php
+++ laravel-server/database/migrations/1_create_users_table.php
@@ -28,7 +28,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->uuid('id')->primary();
-            $table->string('username');
+            $table->string('username')->unique();
             $table->string('email')->unique();
             $table->string('password');
             $table->string('pfp_url')->nullable();
