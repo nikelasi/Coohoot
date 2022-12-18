e4b59152d1302dd8aaa684e005f387b1def5d6a7
NicholasJohansan <nicholasjohansan@gmail.com>
2022-12-18 23:07:00 +0800

    Add validate abstraction to base controller

--- laravel-server/app/Http/Controllers/Controller.php
+++ laravel-server/app/Http/Controllers/Controller.php
@@ -7,7 +7,29 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+use Illuminate\Support\Facades\Validator;
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function validate(Request $request, array $rules, $messages = [], $customAttributes = []) {
+        
+        $validator = Validator::make(
+            $request->only(array_keys($rules)),
+            $rules,
+            $messages,
+            $customAttributes
+        );
+
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Validation failed.',
+                'errors' => $validator->errors()
+            ], 400);
+        }
+        
+    }
+
 }
