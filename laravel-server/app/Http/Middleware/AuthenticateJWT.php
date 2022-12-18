e591279b71516ed22cf0ed8dd12a67c4e27afc08
NicholasJohansan <nicholasjohansan@gmail.com>
2022-12-18 23:48:24 +0800

    Add JWT Auth middleware

---
+++ laravel-server/app/Http/Middleware/AuthenticateJWT.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class AuthenticateJWT {
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next) {
+        if (!auth()->user()) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Unauthorized'
+            ], 401);
+        }
+        return $next($request);
+    }
+}
