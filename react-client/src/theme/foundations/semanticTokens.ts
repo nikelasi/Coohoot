de77ca6dfc12fee98e9cbced1f75999c189b1518
NicholasJohansan <nicholasjohansan@gmail.com>
2022-12-23 10:33:18 +0800

    Add highlight semantic color token

--- react-client/src/theme/foundations/semanticTokens.ts
+++ react-client/src/theme/foundations/semanticTokens.ts
@@ -4,6 +4,10 @@ const semanticTokens = {
     brand: {
       default: '#136CD7'
     },
+    highlight: {
+      default: '#F2F2F2',
+      _dark: '#303b52'
+    },
     'brand.accent': {
       default: '#1059B1',
       _dark: "#1880FF"
