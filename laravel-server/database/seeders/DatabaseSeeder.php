ab69c38af21f032b9a5a9208c4cf212a2c13cef2
NicholasJohansan <nicholasjohansan@gmail.com>
2023-02-09 19:10:57 +0800

    Add a seeder with nonsense

--- laravel-server/database/seeders/DatabaseSeeder.php
+++ laravel-server/database/seeders/DatabaseSeeder.php
@@ -14,11 +14,39 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $user = new \App\Models\User();
+        $user->username = "math";
+        $user->email = "math@nj.sg";
+        $user->password = \Hash::make("password");
+        $user->pfp_url = "https://coohoot.nj.sg/cloud/default_pfp.png";
+        $user->verified = true;
+        $user->save();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        for ($i = 0; $i < 48; $i++) {
+            $quiz = new \App\Models\Quiz();
+            $quiz->title = "Math Qn " . $i;
+            $quiz->thumbnail_url = "https://coohoot.nj.sg/cloud/default_thumbnail.png";
+            $quiz->description = "This is quiz " . $i;
+            $quiz->visibility = "public";
+            $quiz->published = true;
+            $quiz->owner_id = $user->id;
+            $quiz->save();
+
+            $num1 = rand(1, 100);
+            $num2 = rand(1, 100);
+            $sum = $num1 + $num2;
+
+            $question = new \App\Models\Question();
+            $question->quiz_id = $quiz->id;
+            $question->question = "What is ".$num1." + ".$num2."?";
+            $question->type = "short-answer";
+            $question->time = 20;
+            $question->image_url = null;
+            $question->prev_question = null;
+            $question->options = [];
+            $question->answers = ["/^".$sum."$/"];
+            $question->save();
+        }
     }
+
 }
